36c0e68cef6fd8fb5041e55f1e593687ee5f69e8
Shaun Kinsella <shaun.kinsella38@mail.dcu.ie>
2017-11-14 13:33:42 +0000

    Initial

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+    /*El Gamal key pair
+    //generate x
+    x 1 < x < p-1
+    //generate y
+    y = g^x(mod p)
+    */
+
+    /* Signing message
+    ** choose random k 0 < k < p-1 and gcd(k,p-1) = 1
+    *  r = g^k(mod p)
+    *
+    *  compute s = (H-sha-256(m)-xr)k^-1(mod p-1)
+    *  if s = 0 start again
+     */
+
+    /*
+     *  gcd algo
+     */
+
+    // sign checker
+    
+    public static void main (String [] args)
+    {
+
+    }
+}
